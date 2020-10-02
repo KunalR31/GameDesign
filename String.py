3254fc53eee0ea5f8b9030386868e495c988ec71
Rai <raik24@greenhill.org>
2020-10-02 11:28:25 -0500

    10/2/20

--- String.py
+++ String.py
@@ -2,7 +2,9 @@
 strVar= "Here are the instructions to install Drivers 1. After the download is completed go to where you saved the folder.(By default everything you download from the Internet is saved to the Downloads folder) 2. Right click on the folder and choose ''Extract All'' and then choose ''Extract'' again.  3. Once all the contents have been extracted you may delete/disregard the folder with the zip icon. 4. Next, open and Run the SETUP file. (In most cases it is a setup .exe file OR one listed below: * setup application *Asussetup *pnpinstal64 *pnputil *Igxpin 5. Please choose to 'repair' or 'update' the existing installation (driver) IF any one of those options do appear during the setup."
 print(len(strVar))
 print(strVar.replace('Extract','EXTRACT'))
-print(strVar.replace('setup','SETUP'))
+print(strVar.replace("setup",'SETUP'))
+print(strVar.count("Drivers"))
+print(strVar.find(\n))
 #Finding the 1
 print(strVar.find('4'))
 print(strVar.find('1.'))
