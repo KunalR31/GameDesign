54ccd91d500faed45b8530397182c4d6127cbe03
KunalR31 <kxr3678gmail.com>
2020-12-11 11:28:26 -0600

    Update

--- PygameWindow.py
+++ PygameWindow.py
@@ -1,14 +1,3 @@
-# import pygame
-# import sys, time
-# pygame.init()
-# width = (1000)
-# height = (1000)
-# color = input('What color will the window be? ')
-# if color == 'red':
-#     screen.fill(255,0,0)
-# screen = pygame.display.set_mode((width, height))
-# screen.fill(color)
-# pygame.display.flip()
 import pygame, sys
 pygame.init()
 screen = pygame.display.set_mode([800,600])
